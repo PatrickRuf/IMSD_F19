c389d8a6ea3130d9b6c675cec172f9e171074bca
Patrick Rufino <patrick.rufino@gmail.com>
2019-10-20 10:55:52 -0400

    Added HW week 7
    ignore class example, for me mostly.

---
+++ Class Example 10-16/Assets/ReferenceOtherStuff.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReferenceOtherStuff : MonoBehaviour
+{
+
+    //Mathf.Sin(timer)
+    //private float startTimer;
+    //startTimer += Time.deltaTime;
+    //if (startTimer >= 5)
+        //timer += Time.deltaTime;
+     
+
+    public float timer;
+    public Light lightchangingtored;
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 5)
+        {
+            //float r = (Mathf.Sin(timer) / 2) + .5f;
+            lightchangingtored.color = new Color(Mathf.Sin(timer), 0, 0);
+        }
+    }
+}
---
+++ HW Week 7/Assets/ObjectsMoving.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsMoving : MonoBehaviour
+{
+    public GameObject Cube;
+    public GameObject Sphere;
+    public GameObject Capsule;
+    public Light capsulechangingcolor;
+
+    public float speed;
+    public float timer;
+
+
+    void Start()
+    {
+        speed = 1;
+
+        Cube = Instantiate(Cube, new Vector3(-2, -2, 0), Quaternion.identity);
+        Sphere = Instantiate(Sphere, new Vector3(2, 2, 0), Quaternion.identity);
+        Capsule = Instantiate(Capsule, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        Cube.transform.position = new Vector3(Mathf.Sin(timer) * -4, -2, 0);
+
+        Sphere.transform.position = new Vector3(Mathf.Sin(timer * -1) * -4, 2, 0);
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            Capsule.transform.position += new Vector3(1 * Time.deltaTime * speed, 0, 0);
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            Capsule.transform.position += new Vector3(-1 * Time.deltaTime * speed, 0, 0);
+
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            speed = speed + .5f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            speed = speed - .5f;
+        }
+
+        colorchange();
+    }
+
+    public void colorchange()
+    {
+        if (Capsule.transform.position.x > 6)
+        {
+            capsulechangingcolor.color = new Color(1, 0, 0);
+        }
+
+        if (Capsule.transform.position.x > 6 && speed > 15)
+        {
+            capsulechangingcolor.color = new Color(1, 0, 1);
+        }
+
+        if (Capsule.transform.position.x > 6 && speed < -15)
+        {
+            capsulechangingcolor.color = new Color(.5f, .5f, .5f);
+        }
+
+        if (Capsule.transform.position.x < -6)
+        {
+            capsulechangingcolor.color = new Color(0, 1, 0);
+        }
+
+        if (Capsule.transform.position.x < -6 && speed > 15)
+        {
+            capsulechangingcolor.color = new Color(1, 1, 0);
+        }
+
+        if (Capsule.transform.position.x < -6 && speed < -15)
+        {
+            capsulechangingcolor.color = new Color(0, 0, 1);
+        }
+    }
+}
